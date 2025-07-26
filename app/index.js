6b4bf87b0dfb5a780b3d055910a204d325f95960
thepixelpunk <random.motive.me@gmail.com>
2025-07-26 20:36:27 +0200

    Ready to deploy v2

--- app/index.js
+++ app/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
-    console.log('Google Client ID:', process.env.GOOGLE_CLIENT_ID);
+    
 app.use(session({
   secret: process.env.SESSION_KEY,
   resave: false,
